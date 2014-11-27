0a96f97fc82fc680bc8b4b90cc0cdbd6d8203fa0
nmcristian <nmcristian@gmail.com>
2014-11-27 14:38:29 +0100

    removed some code from REST_Admin_API so it won't give errors at run

--- server/routes/REST_Admin_API.js
+++ server/routes/REST_Admin_API.js
@@ -2,24 +2,27 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var user = mongoose.model('User');
+//var user = mongoose.model('User');
 
 /* GET A User From The DataBase */
 router.get('/user', function(req, res) {
-  if(typeof global.mongo_error !== "undefined"){
-    res.status(500);
-    res.end("Error: "+global.mongo_error+" To see a list of users here, make sure you have started the database and set up some test users (see model-->db.js for instructions)");
-    return;
-  }
-  user.find({}, function (err, users) {
-    if (err) {
-      res.status(err.status || 400);
-      res.end(JSON.stringify({error: err.toString()}));
-      return;
-    }
+//  if(typeof global.mongo_error !== "undefined"){
+//    res.status(500);
+//    res.end("Error: "+global.mongo_error+" To see a list of users here, make sure you have started the database and set up some test users (see model-->db.js for instructions)");
+//    return;
+//  }
+//  user.find({}, function (err, users) {
+//    if (err) {
+//      res.status(err.status || 400);
+//      res.end(JSON.stringify({error: err.toString()}));
+//      return;
+//    }
+//    res.header("Content-type","application/json");
+//    res.end(JSON.stringify(users));
+//  });
+
     res.header("Content-type","application/json");
-    res.end(JSON.stringify(users));
-  });
+    res.end('{"msg" : "Test Message, You are logged on as a User since you could fetch this data"}');
 });
 
 module.exports = router;
