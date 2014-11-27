634b6826fc311948ca03faf7f97c7a11a90a1bdd
smarandadungeanu <smaranda.dungeanu@gmail.com>
2014-11-27 22:46:57 +0100

    now we are able to see all the classes, the semesters and the periods. we can also add semesters and periods.

--- public/app/view3/view3.js
+++ public/app/view3/view3.js
@@ -7,25 +7,9 @@ angular.module('myAppRename.view3', ['ngRoute'])
     templateUrl: 'app/view3/view3.html',
     controller: 'View3Ctrl'
   });
-}])
+}]);
 
-.controller('View3Ctrl', function ($scope, $http) {
-    $http({
-      method: 'GET',
-      url: 'adminApi/user'
-    }).
-      success(function (data, status, headers, config) {
-        $scope.users = data;
-         $scope.error = null;
-      }).
-      error(function (data, status, headers, config) {
-        if(status == 401){
-          $scope.error ="You are not authenticated to request these data";
-            return;
-        }
-        $scope.error = data;
-      });
-});
+
 
 
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
+var data = require('../model/dataLayer');
 
 
 
@@ -50,6 +51,143 @@ router.post('/authenticate', function (req, res) {
 router.get('/partials/:partialName', function(req, res) {
   var name = req.params.partialName;
   res.render('partials/' + name);
+});
+
+router.get('/teachers', function(req,res) {
+    data.getAllTeachers(function(err, teachers){
+        if(err){
+            return err;
+        }
+        res.end(JSON.stringify(teachers));
+    })
+});
+
+router.get('/teachers/:teacherId', function(req,res) {
+    data.getTeacherById(req.params.teacherId, function(err, teacher){
+        if(err){
+            return err;
+        }
+        res.end(JSON.stringify(teacher));
+    })
+});
+
+router.get('/classes/teacher/:teacherId', function(req, res){
+    data.getTeacherById(req.params.teacherId, function(err, teacher){
+        if(err){
+            return err;
+        }
+        data.getClassesForTeacher(teacher, function(err, classes){
+            if(err){
+                return err;
+            }
+            res.end(JSON.stringify(classes));
+        })
+    });
+});
+
+router.get('/classes', function(req,res){
+    data.getAllClasses(function(err, classes){
+        if(err){
+            return err;
+        }
+        res.end(JSON.stringify(classes));
+    })
+});
+
+router.get('/classes/:classId', function(req,res){
+    data.getClassById(req.params.classId, function(err, classs){
+        if(err){
+            console.log(err);
+            return err;
+        }
+        res.end(JSON.stringify(classs));
+    })
+});
+
+router.get('/semesters', function(req,res){
+    data.getAllSemesters(function(err, semesters){
+        if(err){
+            return err;
+        }
+        res.end(JSON.stringify(semesters));
+    })
+});
+
+router.get('/semesters/:semId', function(req,res){
+    data.getSemesterById(req.params.semId, function(err, semester){
+        if(err){
+            return err;
+        }
+        res.end(JSON.stringify(semester));
+    })
+});
+
+router.get('/semesters/class/:clsId', function(req,res){
+    data.getClassById(req.params.clsId, function(err, cls){
+        if(err){
+            return err;
+        }
+        data.getSemestersOfClass(cls, function(err, semesters){
+            if(err){
+                return err;
+            }
+            res.end(JSON.stringify(semesters));
+        });
+    })
+});
+
+router.get('/periods/semester/:semId', function(req,res){
+    data.getSemesterById(req.params.semId, function(err, semester){
+        if(err){
+            return err;
+        }
+        data.getPeriodsOfSemester(semester, function(err, periods){
+            if(err){
+                return err;
+            }
+            res.end(JSON.stringify(periods));
+        });
+    })
+});
+
+router.post('/semester/:name/:startingDate/:endingDate', function(req, res){
+    var semester = {
+        name: req.params.name,
+        startingDate: req.params.startingDate,
+        endingDate: req.params.endingDate
+    };
+    data.createNewSemester(semester, function(err, semester){
+        if(err){
+            return err;
+        }
+        res.end(JSON.stringify(semester));
+    })
+});
+
+router.post('/period/:semId/:name/:maxPoints/:reqPoints', function(req, res){
+    var period = {
+        name: req.params.name,
+        maxPoints: req.params.maxPoints,
+        reqPoints: req.params.reqPoints
+    };
+    data.createNewPeriod(period, function(err, period){
+        if(err){
+            return err;
+        }
+        data.getSemesterById(req.params.semId, function(err, semester){
+            if(err){
+                return err;
+            }
+            semester.periodIds.push({pid: period._id});
+            console.log("Period id added to semester: "+semester.periodIds[0].pid+" semester name: "+semester.name +" semester id: "+semester._id);
+            data.updateSemester(semester.toJSON(), function(err, updatedSemester){
+                if(err){
+                    return err;
+                }
+                res.end(JSON.stringify(period));
+            })
+        })
+    })
 });
 
 module.exports = router;
