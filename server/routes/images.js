5daf8c1d78c3a8ebd11235dda55759768ca40978
nmcristian <nmcristian@gmail.com>
2014-12-14 03:27:10 +0100

    made the upload file option visible only for students fixed uploading a file in images.js

--- server/routes/images.js
+++ server/routes/images.js
@@ -8,17 +8,15 @@ path = require('path');
 
 router.post('/:userId', function(req, res) {
     var userId = req.params.userId;
+    var new_location = 'public/users/' + userId + '/';
     var form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files) {
-//        it just needs a redirect somewhere...
-        res.redirect('/#/view2/' + userId);
-        //res.writeHead(200, {'content-type': 'text/plain'});
-        //res.write('received upload:\n\n');
-        //res.end(util.inspect({fields: fields, files: files}));
-    });
 
-//    shitty thing to work on some more, to make the check about the file to be uploaded
-//    form.on('start')
+//    so it works to check the file type at the beginning of the download:
+//    form.on('fileBegin', function(name, file) {
+//        if (file.type.match(/image/g)) {
+//            console.log(file.type + " => " + file.type.match(/image/g));
+//        }
+//    });
 
     form.on('progress', function(bytesReceived, bytesExpected) {
         var percent_complete = (bytesReceived / bytesExpected) * 100;
@@ -29,26 +27,29 @@ router.post('/:userId', function(req, res) {
         console.error(err);
     });
 
-    form.on('end', function(fields, files) {
-        /* Temporary location of our uploaded file */
-        var temp_path = this.openedFiles[0].path;
-
-        /* The file name of the uploaded file */
-//        var file_name = this.openedFiles[0].name;
-//        console.log('Check dis out: ' + path.extname(this.openedFiles[0].name));
-
-        var file_name = 'avatar' + path.extname(this.openedFiles[0].name);
-
-        /* Location where we want to copy the uploaded file */
-        var new_location = 'public/users/' + userId + '/';
-
-        fs.copy(temp_path, new_location + file_name, function(err) {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log("success!")
+    form.on('end', function() {
+        this.openedFiles.forEach(function(fileToUpload) {
+            if(path.extname(fileToUpload.name).match(/\.(jpg)$/)) {
+//                alternative for fileToUpload.name:   'avatar' + path.extname(this.openedFiles[0].name)
+                fs.copy(fileToUpload.path, new_location + 'avatar.jpg', function (err) {
+                    if (err) {
+                        console.error("error saving " + fileToUpload.name + ": " + err);
+                    } else {
+                        console.log(fileToUpload.name + " saved successfully!")
+                    }
+                });
             }
         });
+    });
+
+    form.parse(req, function(err, fields, files) {
+//        it just needs a redirect somewhere...
+        res.redirect('/#/view2/' + userId);
+
+//        or
+//        res.writeHead(200, {'content-type': 'text/plain'});
+//        res.write('received upload:\n\n');
+//        res.end(util.inspect({fields: fields, files: files}));
     });
 });
 
