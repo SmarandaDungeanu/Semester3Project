1fbb395e5c53ab973f0bfba4643e4af527894428
theIncredibleMarek <marek.furak+github@gmail.com>
2014-12-15 03:22:17 +0100

    Testing

--- test/backend-tests/restAPITests.js
+++ test/backend-tests/restAPITests.js
@@ -7,47 +7,209 @@ var http = require("http");
 var testPort = 9999;
 var testServer;
 var mongoose = require("mongoose");
-var User = mongoose.model("User");
+var teacher = mongoose.model('teacher');
+var student = mongoose.model('student');
+var classes = mongoose.model('class');
+var semester = mongoose.model('semester');
+var period = mongoose.model('period');
+var task = mongoose.model('task');
 
-describe('REST API for /user', function () {
-  //Start the Server before the TESTS
-  before(function (done) {
 
-    testServer = app.listen(testPort, function () {
-      console.log("Server is listening on: " + testPort);
-      done();
-    })
-    .on('error',function(err){
-        console.log(err);
-      });
-  })
+describe('REST API for /teacher', function () {
+    //Start the Server before the TESTS
+    before(function (done) {
 
-  beforeEach(function(done){
-    User.remove({}, function ()
-    {
-      var array = [{userName : "Lars", email :"lars@a.dk",pw: "xxx"},{userName : "Henrik", email :"henrik@a.dk",pw: "xxx"}];
-      User.create(array,function(err){
-        done();
-      });
+        testServer = app.listen(testPort, function () {
+            console.log("Server is listening on: " + testPort);
+            done();
+        })
+            .on('error', function (err) {
+                console.log(err);
+            });
     });
-  })
 
-  after(function(){  //Stop server after the test
-    //Uncomment the line below to completely remove the database, leaving the mongoose instance as before the tests
-    //mongoose.connection.db.dropDatabase();
-    testServer.close();
-  })
+    beforeEach(function (done) {
+        teacher.remove({}, function () {
+            var array = [{
+                fName: "Lars",
+                lName: "Mortensen",
+                email: "lars@a.dk",
+                username: "lars",
+                role: "teacher"
+            }, {
+                fName: "Testing",
+                lName: "Teacher",
+                email: "what@a.dk",
+                username: "test",
+                role: "teacher"
+            }];
+            teacher.create(array, function (err) {
+                done();
+            });
+        });
 
-  it("Should get 2 users; Lars and Henrik", function (done) {
-    http.get("http://localhost:"+testPort+"/adminApi/user",function(res){
-      res.setEncoding("utf8");//response data is now a string
-      res.on("data",function(chunk){
-        var n = JSON.parse(chunk);
-        n.length.should.equal(2);
-        n[0].userName.should.equal("Lars");
-        n[1].userName.should.equal("Henrik");
-        done();
-      });
-    })
-  });
+
+    });
+
+    after(function () {  //Stop server after the test
+        //Uncomment the line below to completely remove the database, leaving the mongoose instance as before the tests
+        //db.dropDatabase();
+        testServer.close();
+    });
+
+    it("Should get 2 teachers", function (done) {
+        http.get("http://localhost:" + testPort + "/teachers", function (res) {
+            res.setEncoding("utf8");//response data is now a string
+            res.on("data", function (chunk) {
+                var n = JSON.parse(chunk);
+                n.length.should.equal(2);
+                n[0].username.should.equal("lars");
+                n[1].username.should.equal("test");
+                done();
+            });
+        })
+    });
+
+    it("Should get teacher by username", function (done) {
+        http.get("http://localhost:" + testPort + "/teachers/username/lars", function (res) {
+            res.setEncoding("utf8");//response data is now a string
+            res.on("data", function (chunk) {
+                var n = JSON.parse(chunk);
+                n.username.should.equal("lars");
+                done();
+            });
+        })
+    });
+});
+
+
+describe('REST API for /student', function () {
+    //Start the Server before the TESTS
+    before(function (done) {
+
+        testServer = app.listen(testPort, function () {
+            console.log("Server is listening on: " + testPort);
+            done();
+        })
+            .on('error', function (err) {
+                console.log(err);
+            });
+    });
+
+    beforeEach(function (done) {
+        student.remove({}, function () {
+            var array = [{
+                fName: "First",
+                lName: "Student",
+                email: "lala@a.dk",
+                username: "student",
+                role: "student"
+            }, {
+                fName: "Testing",
+                lName: "Student",
+                email: "what@a.dk",
+                username: "test",
+                role: "student"
+            }];
+            student.create(array, function (err) {
+                done();
+            });
+        });
+
+
+    });
+
+    after(function () {  //Stop server after the test
+        //Uncomment the line below to completely remove the database, leaving the mongoose instance as before the tests
+        //db.dropDatabase();
+        testServer.close();
+    });
+
+    it("Should get 2 students", function (done) {
+        http.get("http://localhost:" + testPort + "/students", function (res) {
+            res.setEncoding("utf8");//response data is now a string
+            res.on("data", function (chunk) {
+                var n = JSON.parse(chunk);
+                n.length.should.equal(2);
+                n[0].username.should.equal("student");
+                n[1].username.should.equal("test");
+                done();
+            });
+        });
+    });
+
+    it("Should get student by username", function (done) {
+        http.get("http://localhost:" + testPort + "/students/username/student", function (res) {
+            res.setEncoding("utf8");//response data is now a string
+            res.on("data", function (chunk) {
+                var n = JSON.parse(chunk);
+                n.username.should.equal("student");
+                done();
+            });
+        })
+    });
+});
+
+
+describe('REST API for /class', function () {
+    //Start the Server before the TESTS
+    before(function (done) {
+
+        testServer = app.listen(testPort, function () {
+            console.log("Server is listening on: " + testPort);
+            done();
+        })
+            .on('error', function (err) {
+                console.log(err);
+            });
+    });
+
+    beforeEach(function (done) {
+        classes.remove({}, function () {
+            var array = [{name: "First"}, {name: "Second"}, {name: "Testing"}];
+            classes.create(array, function (err) {
+                done();
+            });
+        });
+
+
+    });
+
+    after(function () {  //Stop server after the test
+        //Uncomment the line below to completely remove the database, leaving the mongoose instance as before the tests
+        //db.dropDatabase();
+        testServer.close();
+    });
+
+    it("Should get 3 classes", function (done) {
+        http.get("http://localhost:" + testPort + "/classes", function (res) {
+            res.setEncoding("utf8");//response data is now a string
+            res.on("data", function (chunk) {
+                var n = JSON.parse(chunk);
+                n.length.should.equal(3);
+                n[0].name.should.equal("First");
+                n[1].name.should.equal("Second");
+                n[2].name.should.equal("Testing");
+                done();
+            });
+        })
+    });
+
+    it("Should get class by ID", function (done) {
+        http.get("http://localhost:" + testPort + "/classes", function (res) {
+            res.setEncoding("utf8");//response data is now a string
+            res.on("data", function (chunk) {
+                var n = JSON.parse(chunk);
+                var id = n[0]._id;
+                http.get("http://localhost:" + testPort + "/classes/" + id, function (res) {
+                    res.setEncoding("utf8");//response data is now a string
+                    res.on("data", function (chunk) {
+                        var n = JSON.parse(chunk);
+                        n.name.should.equal('First');
+                        done();
+                    });
+                })
+            });
+        })
+    });
 });
